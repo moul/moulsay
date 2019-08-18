112f761d5182f474a44a0e0af309e0d23401089f
Manfred Touron <m@42.am>
2019-08-18 11:30:24 +0200

    chore: basic CLI

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/eidolon/wordwrap v0.0.0-20161011182207-e0f54129b8bb
+	gopkg.in/urfave/cli.v2 v2.0.0-20190806201727-b62605953717
 	moul.io/asciimoul v1.1.0
 )
--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main // import "moul.io/moulsay"
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
+	cli "gopkg.in/urfave/cli.v2"
 	"moul.io/moulsay/moulsay"
 )
 
-const totalMaxWidth = 72
-
 func main() {
-	fmt.Println(moulsay.Say(strings.Join(os.Args[1:], " "), totalMaxWidth))
+	app := cli.App{
+		Flags: []cli.Flag{
+			&cli.IntFlag{Name: "max-width", Aliases: []string{"w"}, Value: 72},
+		},
+		Action: run,
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(c *cli.Context) error {
+	fmt.Println(moulsay.Say(strings.Join(c.Args().Slice(), " "), c.Int("max-width")))
+	return nil
 }
